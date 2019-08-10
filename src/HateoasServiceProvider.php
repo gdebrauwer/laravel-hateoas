c6e29ae97f4c1146066e7b83a09079c81c538344
GuntherDebrauwer <22586858+GuntherDebrauwer@users.noreply.github.com>
2019-08-10 16:12:40 +0200

    Bind Formatter interface to DefaultFormatter clas

--- src/HateoasServiceProvider.php
+++ src/HateoasServiceProvider.php
@@ -4,6 +4,8 @@ namespace GDebrauwer\Hateoas;
 
 use GDebrauwer\Hateoas\HateoasManager;
 use Illuminate\Support\ServiceProvider;
+use GDebrauwer\Hateoas\Formatters\Formatter;
+use GDebrauwer\Hateoas\Formatters\DefaultFormatter;
 
 class HateoasServiceProvider extends ServiceProvider
 {
@@ -15,5 +17,7 @@ class HateoasServiceProvider extends ServiceProvider
         $this->app->singleton('hateoas', function () {
             return $this->app->make(HateoasManager::class);
         });
+
+        $this->app->bind(Formatter::class, DefaultFormatter::class);
     }
 }
--- tests/HateaosServiceProviderTest.php
+++ tests/HateaosServiceProviderTest.php
@@ -3,6 +3,8 @@
 namespace GDebrauwer\Hateoas\Tests;
 
 use GDebrauwer\Hateoas\HateoasManager;
+use GDebrauwer\Hateoas\Formatters\Formatter;
+use GDebrauwer\Hateoas\Formatters\DefaultFormatter;
 
 class HateaosServiceProviderTest extends TestCase
 {
@@ -10,5 +12,11 @@ class HateaosServiceProviderTest extends TestCase
     public function it_registers_singleton_for_hateaos_manager()
     {
         $this->assertInstanceOf(HateoasManager::class, app('hateoas'));
+    }
+
+    /** @test */
+    public function it_binds_formatter_interface_to_default_formatter_class()
+    {
+        $this->assertInstanceOf(DefaultFormatter::class, app(Formatter::class));
     }
 }
